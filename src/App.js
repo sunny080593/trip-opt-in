fd4b423b6a40b7e419e057099fa72e4cce558065
Sandeep Singh <sunny9969888992@gmail.com>
2021-08-05 20:34:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
  <Navbar/>
 <div styleName="photoDiv">
-     <img src={trip} alt="Trips" className="photo"/>
+     <img  src='https://i.im.ge/2021/08/05/hdO8L.jpg' alt="Trips" className="photo"/>
 </div>
    
 <div styleName="textDiv">
